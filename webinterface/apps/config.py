942f2cbe632c688824b4b45a44cab8cc2ab5fe83
abankalarm <bamalkaranbamal@gmail.com>
2021-12-10 13:54:28 +0530

    FINAL FINAL FINAL

--- webinterface/apps/config.py
+++ webinterface/apps/config.py
@@ -27,11 +27,11 @@ class ProductionConfig(Config):
     # PostgreSQL database
     SQLALCHEMY_DATABASE_URI = '{}://{}:{}@{}:{}/{}'.format(
         config('DB_ENGINE', default='postgresql'),
-        config('DB_USERNAME', default='appseed'),
+        config('DB_USERNAME', default='root'),
         config('DB_PASS', default='pass'),
         config('DB_HOST', default='localhost'),
         config('DB_PORT', default=5432),
-        config('DB_NAME', default='appseed-flask')
+        config('DB_NAME', default='manthan-flask')
     )
 
 
--- webinterface/flask-black-dashboard-master/apps/authentication/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-from flask import Blueprint
-
-blueprint = Blueprint(
-    'authentication_blueprint',
-    __name__,
-    url_prefix=''
-)
